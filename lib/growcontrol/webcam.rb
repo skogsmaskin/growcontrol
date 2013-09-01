49a5a59df310b73f39d298e3981eef0783a45049
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2013-09-01 23:10:08 +0200

    Set date on webcam pic

--- lib/growcontrol/webcam.rb
+++ lib/growcontrol/webcam.rb
@@ -1,11 +1,13 @@
 require 'hornetseye_v4l2'
 require 'hornetseye_rmagick'
+require 'RMagick'
 
 module GrowControl
 
   class WebCam
 
     include Hornetseye
+    include Magick
 
     attr_reader :video_device, :input
 
@@ -21,7 +23,18 @@ module GrowControl
       temp_file = File.join(file_path, "_webcam.jpeg")
       dest_file = File.join(file_path, "webcam.jpeg")
       image.save_ubytergb(temp_file)
-      File.rename(temp_file, dest_file)
+      img = ImageList.new(temp_file)
+      txt = Draw.new
+      img.annotate(txt, 0,0,0,0, Time.now.to_s) do
+        txt.gravity = Magick::SouthEastGravity
+        self.pointsize = 30
+        self.font_family = 'Arial'
+        self.font_weight = BoldWeight
+        self.stroke = '#000000'
+        self.fill = '#ffffff'
+      end
+      img.write(dest_file)
+      File.delete(temp_file)
     end
 
     def close
